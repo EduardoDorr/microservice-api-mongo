38051b18c8ffa9e843dabbeb0941dced0ddc94e7
Eduardo da Silva Dörr <edudorr@hotmail.com>
2023-03-20 22:21:44 -0300

    Initial Commit

---
+++ Microservices/Controllers/CatalogController.cs
@@ -0,0 +1,105 @@
+﻿using Catalog.API.Entities;
+using Catalog.API.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Microservices.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class CatalogController : ControllerBase
+    {
+        private readonly IProductRepository _repository;
+
+        public CatalogController(IProductRepository repository)
+        {
+            _repository = repository ?? throw new ArgumentNullException(nameof(repository));
+        }
+
+        [HttpGet]
+        [ProducesResponseType(typeof(Product), StatusCodes.Status200OK)]
+        public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
+        {
+            var products = await _repository.GetProducts();
+
+            return Ok(products);
+        }
+
+        [HttpGet("{id:length(24)}", Name = "GetProduct")]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(typeof(Product), StatusCodes.Status200OK)]
+        public async Task<ActionResult> GetProductById(string id)
+        {
+            var product = await _repository.GetProductById(id);
+
+            if (product == null)
+                return NotFound();
+
+            return Ok(product);
+        }
+
+        [HttpGet]
+        [Route("[action]/{name}", Name = "GetProductsByName")]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(typeof(IEnumerable<Product>), StatusCodes.Status200OK)]
+        public async Task<ActionResult<IEnumerable<Product>>> GetProductsByName(string name)
+        {
+            var products = await _repository.GetProductsByName(name);
+
+            if (products == null)
+                return NotFound();
+
+            return Ok(products);
+        }
+
+        [HttpGet]
+        [Route("[action]/{category}", Name = "GetProductsByCategory")]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(typeof(IEnumerable<Product>), StatusCodes.Status200OK)]
+        public async Task<ActionResult<IEnumerable<Product>>> GetProductsByCategory(string category)
+        {
+            var products = await _repository.GetProductsByCategory(category);
+
+            if (products == null)
+                return BadRequest("Invalid Category");
+
+            return Ok(products);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(typeof(Product), StatusCodes.Status200OK)]
+        public async Task<ActionResult<Product>> CreateProduct([FromBody] Product product)
+        {
+            if (product == null)
+                return BadRequest("Invalid Product");
+
+            await _repository.CreateProduct(product);
+
+            return CreatedAtRoute("GetProduct", new { id = product.Id }, product);
+        }
+
+        [HttpPut]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(typeof(Product), StatusCodes.Status200OK)]
+        public async Task<ActionResult> UpdateProduct([FromBody] Product product)
+        {
+            if (product == null)
+                return BadRequest("Invalid Product");
+
+            var result = await _repository.UpdateProduct(product);
+
+            return Ok(result);
+        }
+
+        [HttpDelete("{id:length(24)}", Name = "DeleteProduct")]
+        [ProducesResponseType(typeof(Product), StatusCodes.Status200OK)]
+        public async Task<ActionResult> DeleteById(string id)
+        {
+            var result = await _repository.DeleteProduct(id);
+
+            return Ok(result);
+        }
+    }
+}
---
+++ Microservices/Data/CatalogContextSeed.cs
@@ -0,0 +1,93 @@
+﻿using MongoDB.Driver;
+using Catalog.API.Entities;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContextSeed
+    {
+        public static void SeedData(IMongoCollection<Product> productCollection)
+        {
+            var existProduct = productCollection.Find(p => true).Any();
+
+            if (!existProduct)
+                productCollection.InsertManyAsync(GetMyProducts());
+        }
+
+        private static IEnumerable<Product> GetMyProducts()
+        {
+            return new List<Product>()
+            {
+                new Product()
+                {
+                    Name = "IPhone X",
+                    Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit." +
+                    "Phasellus vel tempor massa. Ut est est, sodales accumsan laoreet quis, vulputate nec turpis." +
+                    "Morbi nunc risus, lobortis rhoncus tellus sit amet, pulvinar ultricies purus. In eget est lectus." +
+                    "Cras sollicitudin, nulla non sollicitudin varius, turpis odio laoreet nunc, a placerat lorem urna semper enim." +
+                    "Proin at lectus ac ligula interdum faucibus. Lorem ipsum dolor sit amet, consectetur adipiscing elit." +
+                    "Praesent a libero ullamcorper, pulvinar mi sit amet, semper arcu. Fusce lacinia nec velit in hendrerit." +
+                    "Integer volutpat scelerisque nisi. Nulla facilisi.",
+                    Image = "product-1.png",
+                    Price = 950.00M,
+                    Category = "Smartphone",
+                },
+                new Product()
+                {
+                    Name = "IPhone X Pro",
+                    Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit." +
+                    "Phasellus vel tempor massa. Ut est est, sodales accumsan laoreet quis, vulputate nec turpis." +
+                    "Morbi nunc risus, lobortis rhoncus tellus sit amet, pulvinar ultricies purus. In eget est lectus." +
+                    "Cras sollicitudin, nulla non sollicitudin varius, turpis odio laoreet nunc, a placerat lorem urna semper enim." +
+                    "Proin at lectus ac ligula interdum faucibus. Lorem ipsum dolor sit amet, consectetur adipiscing elit." +
+                    "Praesent a libero ullamcorper, pulvinar mi sit amet, semper arcu. Fusce lacinia nec velit in hendrerit." +
+                    "Integer volutpat scelerisque nisi. Nulla facilisi. ",
+                    Image = "product-2.png",
+                    Price = 1150.00M,
+                    Category = "Smartphone",
+                },
+                new Product()
+                {
+                    Name = "Samsung 10",
+                    Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit." +
+                    "Phasellus vel tempor massa. Ut est est, sodales accumsan laoreet quis, vulputate nec turpis." +
+                    "Morbi nunc risus, lobortis rhoncus tellus sit amet, pulvinar ultricies purus. In eget est lectus." +
+                    "Cras sollicitudin, nulla non sollicitudin varius, turpis odio laoreet nunc, a placerat lorem urna semper enim." +
+                    "Proin at lectus ac ligula interdum faucibus. Lorem ipsum dolor sit amet, consectetur adipiscing elit." +
+                    "Praesent a libero ullamcorper, pulvinar mi sit amet, semper arcu. Fusce lacinia nec velit in hendrerit." +
+                    "Integer volutpat scelerisque nisi. Nulla facilisi. ",
+                    Image = "product-3.png",
+                    Price = 650.00M,
+                    Category = "Smartphone",
+                },
+                new Product()
+                {
+                    Name = "Samsung 20",
+                    Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit." +
+                    "Phasellus vel tempor massa. Ut est est, sodales accumsan laoreet quis, vulputate nec turpis." +
+                    "Morbi nunc risus, lobortis rhoncus tellus sit amet, pulvinar ultricies purus. In eget est lectus." +
+                    "Cras sollicitudin, nulla non sollicitudin varius, turpis odio laoreet nunc, a placerat lorem urna semper enim." +
+                    "Proin at lectus ac ligula interdum faucibus. Lorem ipsum dolor sit amet, consectetur adipiscing elit." +
+                    "Praesent a libero ullamcorper, pulvinar mi sit amet, semper arcu. Fusce lacinia nec velit in hendrerit." +
+                    "Integer volutpat scelerisque nisi. Nulla facilisi. ",
+                    Image = "product-4.png",
+                    Price = 900.00M,
+                    Category = "Smartphone",
+                },
+                new Product()
+                {
+                    Name = "Xiaomi Poco",
+                    Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit." +
+                    "Phasellus vel tempor massa. Ut est est, sodales accumsan laoreet quis, vulputate nec turpis." +
+                    "Morbi nunc risus, lobortis rhoncus tellus sit amet, pulvinar ultricies purus. In eget est lectus." +
+                    "Cras sollicitudin, nulla non sollicitudin varius, turpis odio laoreet nunc, a placerat lorem urna semper enim." +
+                    "Proin at lectus ac ligula interdum faucibus. Lorem ipsum dolor sit amet, consectetur adipiscing elit." +
+                    "Praesent a libero ullamcorper, pulvinar mi sit amet, semper arcu. Fusce lacinia nec velit in hendrerit." +
+                    "Integer volutpat scelerisque nisi. Nulla facilisi. ",
+                    Image = "product-5.png",
+                    Price = 550.00M,
+                    Category = "Smartphone",
+                }
+            };
+        }
+    }
+}
